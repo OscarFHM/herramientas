38833473f839a862d6b436804eaad56a35fa9fc5
zero <ofhm01@gmail.com>
2024-03-16 10:10:31 -0500

    para emviara a linux

--- week6/s6c2/HerreraOscar_S6CASA_EDO2orden.cpp
+++ week6/s6c2/HerreraOscar_S6CASA_EDO2orden.cpp
@@ -5,38 +5,40 @@
 #include<cmath>
 
 using namespace std;
-const int N=10000;
+const int N=1000;
 const double k=15;
 const double m=5;
-
-// void RungeKutta (double h,std::array<double,N> x,std::array<double,N> y,std::string name);
+const double v_0=0;
+const double x_0=0.1;
+void RungeKutta (double h,std::array<double,N> t,std::array<double,N> v_x,std::array<double,N> x,std::string name);
 void Euler (double h,std::array<double,N> t,std::array<double,N> v_x,std::array<double,N> x,std::string name);
-// void Error(std::array<double,N> x,std::array<double,N> y,std::string name);
+void Error(std::array<double,N> t,std::array<double,N> x,std::string name);
 
 int main(){
 	double t_fin=10;
 	double h = t_fin/N;
 	std::array<double,N> t;
 	std::array<double,N> v_x;
-	v_x[0]=0;
+	v_x[0]=v_0;
     std::array<double,N> x;
-	x[0]=0.1;
+	x[0]=x_0;
 
 	Euler(h,t,v_x,x,"DatosE2.dat");
-	// RungeKutta(h,x,y,"datosRK.dat");
+	RungeKutta(h,t,v_x,x,"DatosRK2.dat");
 
 return 0;
 }
-// void Error(std::array<double,N> x,std::array<double,N> y,std::string name){
-// ofstream outfile;
-// outfile.open(name);
-// double ey;
-// for(int i=0;i<N;i++){
-// 		ey=exp(-x[i])-y[i];
-// 		outfile << x[i] <<" "<< ey << endl;
-// 		}
-// outfile.close();
-// }
+void Error(std::array<double,N> t,std::array<double,N> x,std::string name){
+ofstream outfile;
+outfile.open(name);
+double y,ey;
+for(int i=0;i<N;i++){
+        y=(v_0/sqrt(k/m)*sin(sqrt(k/m)*t[i])+x_0*cos(sqrt(k/m)*t[i]));
+		ey=y-x[i];//Caso sin amortiguamiento
+		outfile << t[i] <<" "<< ey << endl;
+		}
+outfile.close();
+}
 
 void Euler (double h,std::array<double,N> t,std::array<double,N> v_x,std::array<double,N> x,std::string name){
 ofstream outfile;
@@ -44,29 +46,37 @@ outfile.open(name);
 outfile << t[0] <<" "<<x[0] << endl;		
 for(int i=0;i<N-1;i++){
 		t[i+1]   = t[i]+h;
-		v_x[i+1] = v_x[i]-h*x[i];
+		v_x[i+1] = v_x[i]-k/m*h*x[i];
 		x[i+1]   = x[i]+h*v_x[i];
 		outfile << t[i+1] <<" "<<x[i+1] << endl;
 		}
 outfile.close();
 
-// Error(t,x,"ErrorE.dat");
+Error(t,x,"ErrorE2.dat");
 }
 
-// void RungeKutta (double h,std::array<double,N> x,std::array<double,N> y,std::string name){
-// 	ofstream outfile;
-// 	outfile.open(name);
-// 	outfile << x[0] <<" "<<y[0] << endl;
-// 	double K1,K2,K3,K4;
-// 	for(int i=0;i<N-1;i++){
-// 		K1=h*y[i];
-// 		K2=h*(y[i]+K1/2.0);
-// 		K3=h*(y[i]+K2/2.0);
-// 		K4=h*(y[i]+K3);
-// 		x[i+1]=x[i]+h;
-// 		y[i+1]=y[i]-(1.0/6.0)*(K1+2.0*K2+2.0*K3+K4);
-// 		outfile << x[i+1] <<" "<<y[i+1] << endl;
-// 	}
-// outfile.close();
-// Error(x,y,"ErrorRK.dat");
-// }
+void RungeKutta (double h,std::array<double,N> t,std::array<double,N> v_x,std::array<double,N> x,std::string name){
+	ofstream outfile;
+	outfile.open(name);
+	outfile << t[0] <<" "<<x[0] << endl;
+	double K11,K12,K13,K14,K21,K22,K23,K24;
+	for(int i=0;i<N-1;i++){
+		K11=h*v_x[i];
+		K12=h*(v_x[i]+K11/2.0);
+		K13=h*(v_x[i]+K12/2.0);
+		K14=h*(v_x[i]+K13);
+
+		K21=h*k/m*x[i];
+		K22=h*(k/m*x[i]+K11/2.0);
+		K23=h*(k/m*x[i]+K12/2.0);
+		K24=h*(k/m*x[i]+K13);
+
+		t[i+1]=t[i]+h;
+		v_x[i+1]=v_x[i]+(1.0/6.0)*(K21+2.0*K22+2.0*K23+K24);
+		x[i+1]=x[i]-(1.0/6.0)*(K11+2.0*K12+2.0*K13+K14);
+
+		outfile << t[i+1] <<" "<<x[i+1] << endl;
+	}
+outfile.close();
+Error(t,x,"ErrorRK2.dat");
+}
